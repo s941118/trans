242763f5604a2fa01ebbf2482e1416107d14b08a
Apple <apple@Clouds-MacBook-Pro.local>
2017-07-11 03:34:58 +0800

    0711

--- assets/js/index.js
+++ assets/js/index.js
@@ -36,24 +36,39 @@ $(window).load(function() {
   	$('.container').css('overflow', 'auto');
   })
 
+  //FAQ展開的部分
+	$(".faq-q").click( function () {
+	  var container = $(this).parents(".faq-c");
+	  var answer = container.find(".faq-a");
+	  var trigger = container.find(".faq-t");
+	  
+	  answer.slideToggle(200);
+	  
+	  if (trigger.hasClass("faq-o")) {
+	    trigger.removeClass("faq-o");
+	  }
+	  else {
+	    trigger.addClass("faq-o");
+	  }
+	});
+
+	// CommunityPartner
+	let target 
+	$(window).bind('resize', function () {
+		target = $('#partner_link').offset().top
+	}).resize()
+	$(window).bind('scroll', function () {
+		let whereAmI = $(this).scrollTop()
+		if ( whereAmI < target ) {
+			$('#partner_link').removeClass('fixed')
+		}
+		else if ( whereAmI > target ) {
+			$('#partner_link').addClass('fixed')
+		}
+	}).scroll()
+
 })
 
-
-//FAQ展開的部分
-$(".faq-q").click( function () {
-  var container = $(this).parents(".faq-c");
-  var answer = container.find(".faq-a");
-  var trigger = container.find(".faq-t");
-  
-  answer.slideToggle(200);
-  
-  if (trigger.hasClass("faq-o")) {
-    trigger.removeClass("faq-o");
-  }
-  else {
-    trigger.addClass("faq-o");
-  }
-});
 
 
 // new Vue({
