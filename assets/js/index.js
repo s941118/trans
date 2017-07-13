34fe8dfc23af316479768321673d6b9e37c4e7f5
John Wang <john29917958@gmail.com>
2017-07-14 00:20:39 +0800

    Fix Navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,62 +1,58 @@
-$(window).load(function() {
+$(document).ready(function() {
+    /// 讓頁面跳轉接過場柔順一點
+    $('body').removeClass('hide')
+    $('#nav').on('click', 'a', function() {
+        $('body').addClass('hide')
+    })
 
-	/// 讓頁面跳轉接過場柔順一點
-	$('body').removeClass('hide')
-	$('#nav').on('click', 'a', function() {
-		$('body').addClass('hide')
-	})
+    /// 載入 partial 們
+    // $('#nav').load('../../partail/_nav.html');
+    $('#header').load('../../partail/_header.html', function() {
+        $('button.navbar-toggle').click(function() {
+            $(this).closest('nav').toggleClass('toggled');
+        });
+    });
+    $('#footer').load('../../partail/_footer.html');
 
-	/// 載入 partial 們
-	// $('#nav').load('../../partail/_nav.html');
-	$('#header').load('../../partail/_header.html');
-	$('#footer').load('../../partail/_footer.html');
-
-	/// 漢堡包
-	$('#mobile-nav').click(function() {
-		$('.mobile-nav-btn').toggleClass('active');
-		$('#nav').toggleClass('active');
-	})
-
-
-
-  //FAQ展開的部分
-	$(".faq-q").click( function () {
-	  var container = $(this).parents(".faq-c");
-	  var answer = container.find(".faq-a");
-	  var trigger = container.find(".faq-t");
-	  
-	  answer.slideToggle(200);
-	  
-	  if (trigger.hasClass("faq-o")) {
-	    trigger.removeClass("faq-o");
-	  }
-	  else {
-	    trigger.addClass("faq-o");
-	  }
-	});
-
-	// CommunityPartner
-	let target 
-	
-	$(window).bind('resize', function () {
-		target = $('#partner_link').offset().top
-	}).resize()
-
-	$(window).bind('scroll', function () {
-		let whereAmI = $(this).scrollTop()
-		if ( whereAmI < target ) {
-			$('#partner_link').removeClass('fixed')
-		}
-		else if ( whereAmI > target ) {
-			$('#partner_link').addClass('fixed')
-		}
-	}).scroll()
+    /// 漢堡包
+    $('#mobile-nav').click(function() {
+        $('.mobile-nav-btn').toggleClass('active');
+        $('#nav').toggleClass('active');
+    })
 
 
 
-	
+    //FAQ展開的部分
+    $(".faq-q").click(function() {
+        var container = $(this).parents(".faq-c");
+        var answer = container.find(".faq-a");
+        var trigger = container.find(".faq-t");
 
-})
+        answer.slideToggle(200);
+
+        if (trigger.hasClass("faq-o")) {
+            trigger.removeClass("faq-o");
+        } else {
+            trigger.addClass("faq-o");
+        }
+    });
+
+    // CommunityPartner
+    let target
+
+    $(window).bind('resize', function() {
+        target = $('#partner_link').offset().top
+    }).resize()
+
+    $(window).bind('scroll', function() {
+        let whereAmI = $(this).scrollTop()
+        if (whereAmI < target) {
+            $('#partner_link').removeClass('fixed')
+        } else if (whereAmI > target) {
+            $('#partner_link').addClass('fixed')
+        }
+    }).scroll();
+});
 
 
 
